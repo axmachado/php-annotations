d3e81faf93f9a827a3be85ceb5645cc819ba2740
Luís <lcobucci@gmail.com>
2011-08-12 18:51:57 -0300

    Removed runtime dir

--- demo/runtime/RequiredAnnotation.php-6e037926.annotations.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array(
-  'Mindplay\\Annotation\\Standard\\RequiredAnnotation' => array(
-    array('Mindplay\\Annotation\\Core\\UsageAnnotation', 'property'=>true, 'inherited'=>true)
-  ),
-);
--- demo/runtime/ValidationAnnotationBase.php-128917a0.annotations.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array(
-  'Mindplay\\Annotation\\Standard\\ValidationAnnotationBase::$message' => array(
-    array('Mindplay\\Annotation\\Standard\\VarAnnotation', 'type' => 'string')
-  ),
-);
