f0008348e5f36e4314f1e7bc0e1cf81f63f15763
Alexandre Machado <axmachado@gmail.com>
2014-01-15 16:20:25 -0200

    * Correção da verificação de última alteração para funcionar tanto com APC quanto com APCU. * Formatação de acordo com o PSR2.

--- src/Mindplay/Annotation/Cache/ApcCache.php
+++ src/Mindplay/Annotation/Cache/ApcCache.php
@@ -1,95 +1,106 @@
 <?php
+
+/**
+ * ApcCache.php - cache de anotações usando APC/APCU
+ */
+
 namespace Mindplay\Annotation\Cache;
 
 use \Mindplay\Annotation\Core\AnnotationException;
 use \ReflectionClass;
 
+/**
+ * Implementação do cache de anotações usando APC/APCu
+ */
 class ApcCache implements CacheStorage
 {
-	/**
-	 * Class constructor
-	 *
-	 * @throws AnnotationException
-	 */
-	public function __construct()
-	{
-		if (!extension_loaded('apc')) {
-			throw new AnnotationException(
-				'You must have APC extension in order to use the APC cache storage'
-			);
-		}
-	}
 
-	/**
-	 * Returns if the identifier exists on the storage
-	 *
-	 * @param string $id
-	 * @return boolean
-	 */
-	public function exists($id)
-	{
-		return apc_exists($id);
-	}
+    /**
+     * Class constructor
+     *
+     * @throws AnnotationException
+     */
+    public function __construct()
+    {
+        if (!extension_loaded('apc')) {
+            throw new AnnotationException(
+                'You must have APC extension in order to use the APC cache storage'
+            );
+        }
+    }
 
-	/**
-	 * Stores the content
-	 *
-	 * @param string $id
-	 * @param string $content
-	 */
-	public function store($id, $content)
-	{
-		if (apc_store($id, eval($content)) === false) {
-			throw new AnnotationException(__METHOD__ . ' : error writing cache ' . $id);
-		}
-	}
+    /**
+     * Returns if the identifier exists on the storage
+     *
+     * @param string $id
+     * @return boolean
+     */
+    public function exists($id)
+    {
+        return apc_exists($id);
+    }
 
-	/**
-	 * Retrieves the content
-	 *
-	 * @param string $id
-	 * @return mixed
-	 */
-	public function get($id)
-	{
-		$content = apc_fetch($id);
+    /**
+     * Stores the content
+     *
+     * @param string $id
+     * @param string $content
+     */
+    public function store($id, $content)
+    {
+        $contentToStore = eval($content);
+        if (apc_store($id, $contentToStore) === false) {
+            throw new AnnotationException(__METHOD__ . ' : error writing cache ' . $id);
+        }
+    }
 
-		return $content;
-	}
+    /**
+     * Retrieves the content
+     *
+     * @param string $id
+     * @return mixed
+     */
+    public function get($id)
+    {
+        $content = apc_fetch($id);
 
-	/**
-	 * Returns the last change time
-	 *
-	 * @param string $id
-	 * @return int
-	 */
-	public function getLastChangeTime($id)
-	{
-		$info = apc_cache_info('user');
+        return $content;
+    }
 
-		foreach ($info['cache_list'] as $cache) {
-			$key = 'info';
-			if (!array_key_exists($key, $cache)) {
-				$key = 'key';
-			}
-			if (array_key_exists($key, $cache) && $cache[$key] == $id) {
-				return $cache['mtime'];
-			}
-			else {
-				return 0;
-			}
-		}
-	}
+    /**
+     * Returns the last change time
+     *
+     * @param string $id
+     * @return int
+     */
+    public function getLastChangeTime($id)
+    {
+        $info = apc_cache_info('user');
+        if (isset($info) && isset($info['cache_list'])) {
+            $list = $info['cache_list'];
+            foreach ($list as $cache) {
+                $key = "";
+                if (isset($cache['key'])) {
+                    $key = $cache['key'];
+                } elseif (isset($cache['info'])) {
+                    $key = $cache['info'];
+                }
+                if ($key == $id) {
+                    return $cache['mtime'];
+                }
+            }
+        }
+        return 0;
+    }
 
-	/**
-	 * Creates an ID for the storage from class name
-	 *
-	 * @param string $className
-	 * @return string
-	 */
-	public function createId(ReflectionClass $class)
-	{
-		return $class->getName() . '-annotations';
-	}
+    /**
+     * Creates an ID for the storage from class name
+     *
+     * @param string $className
+     * @return string
+     */
+    public function createId(ReflectionClass $class)
+    {
+        return $class->getName() . '-annotations';
+    }
 }
-
